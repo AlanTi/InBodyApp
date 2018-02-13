71596df373a27340fa6cdd24ef7b588564e61aed
alan.zhao <zhaotian198410@hotmail.com>
2018-02-13 21:50:14 +0800

    添加沉浸式标题栏

--- app/src/main/java/com/example/poortiti/inbodyapp/MainActivity.kt
+++ app/src/main/java/com/example/poortiti/inbodyapp/MainActivity.kt
@@ -4,9 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var TAG:String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        StatusBarCompat.compat();
+    }
+
+    fun test(){
+
     }
 }
