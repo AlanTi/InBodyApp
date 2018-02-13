a9b6fa20c0edfd035db446ea9953101c419bbf89
alan.zhao <zhaotian198410@hotmail.com>
2018-02-13 21:48:40 +0800

    沉浸式标题连

---
+++ app/src/main/java/com/example/poortiti/inbodyapp/StatusBarCompat.kt
@@ -0,0 +1,14 @@
+package com.example.poortiti.inbodyapp
+
+import android.app.Activity
+
+/**
+ * Created by zhaotian on 18/2/13.
+ */
+object StatusBarCompat {
+
+    fun compat(activity: Activity, statusColor:Int){
+
+    }
+
+}
